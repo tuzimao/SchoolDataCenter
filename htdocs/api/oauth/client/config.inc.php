0703cb8ce36cedbc748e05b9016a112b4effde3a
School AI <chatbookai@gmail.com>
2025-04-29 10:30:12 -0700

    oauth_access_tokens

--- htdocs/api/oauth/client/config.inc.php
+++ htdocs/api/oauth/client/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$redirect_uri   = 'http://localhost/api/oauth/client/callback.php';
-$client_id      = 'd37d1c43f4cbe10548f80d755c18752f';                                           //From registerClient.php
-$client_secret  = '1e04093586f2a95f20c2ae1404e3385f8802b0e4c323989ce7e5341612a85874';           //From registerClient.php
-$authorize_uri  = 'http://localhost/api/oauth/authorization.php';
+$redirect_uri   = 'http://localhost:8888/api/oauth/client/callback.php';
+$client_id      = 'cfc909901f5d511f19c8e0d00464e394';                                           //From registerClient.php
+$client_secret  = '3f58ec5e8e35c7625786f683620cb477e2e6228aaa1b38d9bbb1e84cd08bd918';           //From registerClient.php
+$authorize_uri  = 'http://localhost:8888/api/oauth/authorization.php';
 
 
 
