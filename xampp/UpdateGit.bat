5ca7f8255b6d48eb6834ebc8771f666ab2129261
School AI <chatbookai@gmail.com>
2025-04-29 10:12:36 -0700

    UpdateGit

--- xampp/UpdateGit.bat
+++ xampp/UpdateGit.bat
@@ -14,8 +14,10 @@ del "D:\SchoolDataCenter\xampp\mysql\data\mysqld.dmp"
 
 rd /S /Q D:\SchoolDataCenter\htdocs\webroot
 
-powershell -Command "Expand-Archive -Path D:\SchoolDataCenter\htdocs\output\webroot.zip -DestinationPath D:\SchoolDataCenter\htdocs -Force"
+REM powershell 5.0
+REM powershell -Command "Expand-Archive -Path D:\SchoolDataCenter\htdocs\output\webroot.zip -DestinationPath D:\SchoolDataCenter\htdocs -Force"
 
+REM powershell 2.0
 powershell -command "$shell = New-Object -ComObject Shell.Application; $zip = $shell.NameSpace('D:\SchoolDataCenter\htdocs\output\webroot.zip'); $dest = $shell.NameSpace('D:\SchoolDataCenter\htdocs'); $dest.CopyHere($zip.Items(), 16)"
 
 
