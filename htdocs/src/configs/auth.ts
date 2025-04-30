fa1371af776f3a264f091c3eb499d447464a826d
School AI <chatbookai@gmail.com>
2025-04-29 21:24:56 -0700

    20250430

--- htdocs/src/configs/auth.ts
+++ htdocs/src/configs/auth.ts
@@ -5,7 +5,6 @@ AppSchoolConfigMap['dandian.net']     = ["https://demoapi.dandian.net/api/", 'Sc
 AppSchoolConfigMap['fdzyzz.com']      = ["https://fdzz.dandian.net:8443/api/", '福鼎职中', "auth/menus.php", "https://fdzz.dandian.net:8443/api/"]
 AppSchoolConfigMap['fjsmnx.com']      = ["https://dsj.fjsmlyxx.com:1443/api/", '三明林业', "auth/menus.php", "https://dsj.fjsmlyxx.com:1443/api/"]
 const AppMarkId   = "dandian.net"; //需要针对每个学校的情况进行修改
-const AppVersion  = '20250430'; //需要刷新客户端JS时需要更新此值
 const APP_URL = AppSchoolConfigMap[AppMarkId][0]
 const AppName = AppSchoolConfigMap[AppMarkId][1]
 const indexMenuspath = AppSchoolConfigMap[AppMarkId][2]
@@ -30,7 +29,7 @@ export const authConfig = {
 export const defaultConfig = {
   Github: 'https://github.com/SmartSchoolAI/SchoolDataCenter',
   Docs: 'https://docs.dandian.net',
-  AppVersion: AppVersion,
+  AppVersion: '20250430',
   AppVersionType: '试用版本',
   defaultLanguage: 'zh',
   storageTokenKeyName: 'accessToken',
