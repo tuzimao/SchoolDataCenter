617e82b52545cdcbfe89506898adbfdc52601eb4
School AI <chatbookai@gmail.com>
2025-04-29 18:36:14 -0700

    Update auth.ts

--- htdocs/src/configs/auth.ts
+++ htdocs/src/configs/auth.ts
@@ -6,8 +6,8 @@ AppSchoolConfigMap['dandian.net']     = ["https://demoapi.dandian.net/api/", 'Sc
 AppSchoolConfigMap['fdzyzz.com']      = ["https://fdzz.dandian.net:8443/api/", '福鼎职中', "auth/menus.php", "https://fdzz.dandian.net:8443/api/"]
 AppSchoolConfigMap['fjsmnx.com']      = ["https://dsj.fjsmlyxx.com:1443/api/", '三明林业', "auth/menus.php", "https://dsj.fjsmlyxx.com:1443/api/"]
 
-const AppMarkId   = "development"; //需要针对每个学校的情况进行修改
-const AppVersion  = '20250425'; //需要刷新客户端JS时需要更新此值
+const AppMarkId   = "dandian.net"; //需要针对每个学校的情况进行修改
+const AppVersion  = '20250430'; //需要刷新客户端JS时需要更新此值
 
 const APP_URL = AppSchoolConfigMap[AppMarkId][0]
 const AppName = AppSchoolConfigMap[AppMarkId][1]
