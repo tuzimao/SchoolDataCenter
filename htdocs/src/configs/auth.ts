df1e19f3d1919a5c0814a7de63cdd8292dfd5132
School AI <chatbookai@gmail.com>
2025-04-30 16:01:55 -0700

    AppMarkId

--- htdocs/src/configs/auth.ts
+++ htdocs/src/configs/auth.ts
@@ -4,16 +4,15 @@ AppSchoolConfigMap['development']     = ["http://localhost:8888/api/", 'SchoolAI
 AppSchoolConfigMap['dandian.net']     = ["https://demoapi.dandian.net/api/", 'SchoolAI', "auth/menus.php", "https://demoapi.dandian.net/api/"] // For School and Demoapi.dandian.net
 AppSchoolConfigMap['fdzyzz.com']      = ["https://fdzz.dandian.net:8443/api/", '福鼎职中', "auth/menus.php", "https://fdzz.dandian.net:8443/api/"]
 AppSchoolConfigMap['fjsmnx.com']      = ["https://dsj.fjsmlyxx.com:1443/api/", '三明林业', "auth/menus.php", "https://dsj.fjsmlyxx.com:1443/api/"]
-const AppMarkId   = "dandian.net"; //需要针对每个学校的情况进行修改
-const APP_URL = AppSchoolConfigMap[AppMarkId][0]
-const AppName = AppSchoolConfigMap[AppMarkId][1]
-const indexMenuspath = AppSchoolConfigMap[AppMarkId][2]
-const backEndApiAiBaseUrl = AppSchoolConfigMap[AppMarkId][3]
+const APP_URL = AppSchoolConfigMap["dandian.net"][0]
+const AppName = AppSchoolConfigMap["dandian.net"][1]
+const indexMenuspath = AppSchoolConfigMap["dandian.net"][2]
+const backEndApiAiBaseUrl = AppSchoolConfigMap["dandian.net"][3]
 
 export const authConfig = {
     AppName: AppName,
-    AppLogo: '/icons/' + AppMarkId + '/icon256.png',
-    AppMarkId: AppMarkId,
+    AppLogo: '/icons/' + "dandian.net" + '/icon256.png',
+    AppMarkId: "dandian.net",
     AppSchoolConfigMap: AppSchoolConfigMap,
     indexMenuspath: indexMenuspath,
     loginEndpoint: APP_URL + 'jwt.php?action=login',
@@ -22,8 +21,8 @@ export const authConfig = {
     registerEndpoint: APP_URL + 'jwt/register',
     backEndApiHost: APP_URL,
     backEndApiAiBaseUrl: backEndApiAiBaseUrl,
-    indexImageUrl: '/images/school/' + AppMarkId + '/index.jpg',
-    logoUrl: '/images/school/' + AppMarkId + '/logo.png'
+    indexImageUrl: '/images/school/' + "dandian.net" + '/index.jpg',
+    logoUrl: '/images/school/' + "dandian.net" + '/logo.png'
 }
 
 export const defaultConfig = {
