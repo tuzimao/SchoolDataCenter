41bd14e589af72ea7d90c038b4a6d41cf71bc4c3
School AI <chatbookai@gmail.com>
2025-06-07 11:24:37 -0700

    webroot

--- htdocs/api/include.inc.php
+++ htdocs/api/include.inc.php
@@ -969,18 +969,37 @@ function decodeBase58($base58) {
 }
 
 function getUserInfoFromWechatServer($code) {
-	$RS 			= [];
-	$RS['openid'] 	=  "o8qXQ65lQmGyK2cB4KQAkWcK4_Zo";
-	$RS['nickname'] =  "王纪云";
-	$RS['sex'] 		=  0;
-	$RS['language'] =  "";
-	$RS['city'] 	=  "";
-	$RS['province'] =  "";
-	$RS['country'] 	=  "";
-	$RS['headimgurl'] 	=  "https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eq57YjNN6fhEoeoMhads3q216W4YnnKq5EuuDhsp88rDaosicJIDmfAqyB3jnYsMvkribcTz3Cpzia6Q/132";
-	$RS['privilege'] 	= [ ];
-	$RS['unionid'] 		=  "ol6PQ66dJE0XNaECZeCHJv6JpJF8";
+
+	$code 	= $_GET['code'];
+	$appid 	= 'wx8731239b834cd9ca';
+	$secret = 'ba73792c174e72a0cb079f30fc6f0ef1';
+
+	// 请求 access_token
+	$url		= "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+	$response 	= file_get_contents($url);
+	$data 		= json_decode($response, true);
+
+	// 获取用户基本信息（可选）
+	$access_token 	= $data['access_token'];
+	$openid 		= $data['openid'];
+	$userInfoUrl 	= "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid";
+	$userInfo 		= json_decode(file_get_contents($userInfoUrl), true);
+
+	return $userInfo;
+	/*
+	$RS 				=  [];
+	$RS['openid'] 		=  "";
+	$RS['nickname'] 	=  "";
+	$RS['sex'] 			=  0;
+	$RS['language'] 	=  "";
+	$RS['city'] 		=  "";
+	$RS['province'] 	=  "";
+	$RS['country'] 		=  "";
+	$RS['headimgurl'] 	=  "";
+	$RS['privilege'] 	=  [];
+	$RS['unionid'] 		=  "";
 	return $RS;
+	*/
 }
 
 if(is_file("function.xmjs.php")) {
